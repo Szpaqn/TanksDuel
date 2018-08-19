3c3e7c994d41850e52baba41b333c9abaccc9c7d
Jakub <jakub.m.michalowski@gmail.com>
2018-08-19 15:35:57 +0200

    V 0.0.3 Empty Unreal 4 project
    Created an empty Unreal 4 project

---
+++ U4Project/Source/U4Project/U4Project.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "U4Project.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, U4Project, "U4Project" );
---
+++ U4Project/Source/U4Project/U4ProjectGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "U4ProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class U4PROJECT_API AU4ProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
